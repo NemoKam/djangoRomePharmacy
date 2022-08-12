a811df2a9c528b2e554d61a06126ade113af48d9
nemoxxxy <kamkamuch.7@gmail.com>
2022-08-12 10:53:52 +0300

    end

--- pharmacy/comms/admin.py
+++ pharmacy/comms/admin.py
@@ -7,11 +7,5 @@ class CommentAdmin(admin.ModelAdmin):
     search_fields = ['text']
     def get_search_results(self, request, queryset, search_term):
         queryset, use_distinct = super().get_search_results(request, queryset, search_term)
-        try:
-            search_term_as_int = int(search_term)
-        except ValueError:
-            pass
-        else:
-            queryset |= self.model.objects.filter(age=search_term_as_int)
         return queryset, use_distinct
 admin.site.register(Comment, CommentAdmin)
--- pharmacy/comms/models.py
+++ pharmacy/comms/models.py
@@ -1,17 +1,10 @@
 from django.db import models
 class Comment(models.Model):
     pharmacyurl = models.TextField()
-    pharmacyname = models.CharField(max_length=100)
+    pharmacyname = models.CharField(max_length=100) #Если бы аптека была в Базе, можно было бы вписать ForeignKey()
     author = models.CharField(max_length=100) #Если бы автор был в Базе, можно было бы вписать ForeignKey()
     sources = models.TextField()
     date = models.CharField(max_length=100)
     text = models.TextField()
     def __str__(self):
         return self.pharmacyname
-class Test(models.Model):
-    pharmacyurl = models.TextField()
-    pharmacyname = models.CharField(max_length=100)
-    author = models.CharField(max_length=100) #Если бы автор был в Базе, можно было бы вписать ForeignKey()
-    sources = models.TextField()
-    date = models.CharField(max_length=100)
-    text = models.TextField()
